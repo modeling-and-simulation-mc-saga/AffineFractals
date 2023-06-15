9a670148c652f34c554583050dae0aa350490465
tada195908 <48470775+tada195908@users.noreply.github.com>
2023-06-15 16:03:55 +0900

    Update TwinChristmasTree.java for quiz

--- src/model/TwinChristmasTree.java
+++ src/model/TwinChristmasTree.java
@@ -10,9 +10,9 @@ public class TwinChristmasTree extends AbstractFractal {
     public void setAffine() {
         double r=1./2.;
         double p = Math.PI/2.;
-        affine.add(parameters(r,r,p,p,r,0.));
-        affine.add(parameters(r,r,0.,0.,r/2.,r));
-        affine.add(parameters(r,r,-p,-p,r,r));
+
+    
+    
     }
 
 }
